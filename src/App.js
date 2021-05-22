9c13558db2339781c2ec4ceeab77ad3d511b8a1e
EC2 Default User <ec2-user@ip-172-31-36-214.us-west-2.compute.internal>
2021-05-22 04:59:10 +0000

    fix layout

--- src/App.js
+++ src/App.js
@@ -12,31 +12,12 @@ class App extends Component {
     title: "",
     isInputName: false,
     isPush: false,
-    imgPath: "https://4.bp.blogspot.com/-Oy1zi2vyIXU/Ws2A8zxW_wI/AAAAAAABLOQ/3O9a2ZvHf0E6VlPYx389BeOhmiNb3i2eACLcBGAs/s800/text_gion_shiin.png"
-  }
-
-  setImage() {
-    if (this.state.posts.length >= 15)
-       return "https://thumb.photo-ac.com/a4/a4802d10fa1308cb3cf17d3cd86b4ce6_t.jpeg"
-    else if(this.state.posts.length >= 10)
-       return "https://thumb.photo-ac.com/5f/5f8e9f4884d70c1316f83e94c9091344_t.jpeg"
-    else if(this.state.posts.length >= 5)
-       return "https://www.aco.co.jp/_file/facility/65204/01.jpg"
-    else if(this.state.posts.length > 1)
-       return "https://www.sozailab.jp/db_img/sozai/39971/2ef314d87e7e422f11cfa6162ca3e2bd.jpg"
-    else
-       return "https://4.bp.blogspot.com/-Oy1zi2vyIXU/Ws2A8zxW_wI/AAAAAAABLOQ/3O9a2ZvHf0E6VlPYx389BeOhmiNb3i2eACLcBGAs/s800/text_gion_shiin.png"
+    nowDrawing: false,
+    question: "聞きたいことは？"
   }
   
-  checkKanpai(content) {
-    // X秒以内に乾杯
-    const diffSeconds = 5
-    if(!content['createdAt'] || this.state.title === '') return false
-    let targetDate = new Date(content.createdAt.substr(0, 19))
-    // 日本時間に合わせる
-    targetDate.setHours(targetDate.getHours() + 9)
-    let diff = (new Date().getTime() / 1000) - (targetDate.getTime() / 1000)
-    return (content.title !== this.state.title && diff < diffSeconds)
+  checkQuestion(content) {
+    return !(content.title in this.state.posts)
   }
   
 
@@ -44,9 +25,15 @@ async componentDidMount() {
     try {
       const posts = await API.graphql(graphqlOperation(listPosts))
       let nameList = posts.data.listPosts.items.filter((post) => {
-        return this.checkKanpai(post)
+        return this.checkQuestion(post)
       })
-      this.setState({ posts: nameList, imgPath: this.setImage() })
+      nameList.sort(function(a,b){
+        if(a.createdAt > b.createdAt) return -1;
+        if(a.createdAt < b.createdAt) return 1;
+        return 0;
+      });
+      console.log('nameList : ', nameList)
+      this.setState({ posts: nameList })
     } catch (e) {
       console.log(e)
     }
@@ -54,26 +41,23 @@ async componentDidMount() {
     API.graphql(graphqlOperation(onCreatePost)).subscribe({
       next: (eventData) => {
         const post = eventData.value.data.onCreatePost
-        let posts = [...this.state.posts.filter(content => {
-          return this.checkKanpai(content)
+        const posts = [...this.state.posts.filter(content => {
+          return this.checkQuestion(content)
         }), post]
-        // subscribの時は直前にmutationした型が異なるため、別途重複削除が必要
-        posts = posts.filter((element, index, self) => 
-          self.findIndex(e => 
-            e.title === element.title
-          ) === index)
-        console.log('length subs : ', this.state.posts.length)
-        this.setState({ posts, imgPath: this.setImage() })
+        posts.sort(function(a,b){
+          if(a.createdAt > b.createdAt) return -1;
+          if(a.createdAt < b.createdAt) return 1;
+          return 0;
+        });
+        this.setState({ posts })
       }
     })
   }
 
   createPost = async () => {
-    this.setState({ isPush: true })
-      
+
     // バリデーションチェック
     if (this.state.title === '') {
-      this.setState({isPush: false })
       return
     }
 
@@ -84,12 +68,8 @@ async componentDidMount() {
 
     // 登録処理
     try {
-      const posts = [...this.state.posts.filter(content => {
-        return this.checkKanpai(content)
-        }), createPostInput]
-      this.setState({ posts: posts, title: this.state.title, imgPath: this.setImage(), isInputName:true, isPush: false })
+      this.setState({ title: ""})
       await API.graphql(graphqlOperation(createPost, { input: createPostInput }))
-      console.log('createPostInput: ', createPostInput)
     } catch (e) {
       console.log(e)
     }
@@ -101,25 +81,44 @@ async componentDidMount() {
     })
   }
 
+  // ルーレットを開始
+  start = () => {
+    let ruoState = setInterval(() => {
+      const maxLen = this.state.posts.length
+      // 0〜maxLenの範囲でランダムな数値を作成
+      var idx = Math.floor( Math.random() * maxLen )
+      // ルーレット
+      this.setState({ question: this.state.posts[idx].title })
+    }, 80);
+    this.setState({ nowDrawing: ruoState })
+  }
+     
+  // ルーレットを停止
+  stop = () => {
+    if(this.state.nowDrawing) {
+      clearInterval(this.state.nowDrawing)
+    }
+    this.setState({ nowDrawing: false })
+  }
+     
   render() {
     return (
       <div className="App">
         <div className='namelabel'>
-          名前
-        <input className='namelabel' value={this.state.title} name="title" maxLength={8} onChange={this.onChange} disabled={this.state.isInputName}></input>
+          質問
+        <input className='nameInput' value={this.state.title} name="title" maxLength={20} onChange={this.onChange}></input>
+        <button className='postButton' onClick={this.createPost} disabled={this.state.nowDrawing}>投稿</button>
         </div>
-        <button className='kanpaiButton' onClick={this.createPost} disabled={this.state.isPush}>カンパ〜い！</button>
+        <div className='mainQuestion'>{this.state.question}
+        </div>
+        <div>
+          <button className='selectButton' onClick={this.start} disabled={this.state.nowDrawing}>スタート</button>
+          <button className='selectButton' onClick={this.stop} disabled={!this.state.nowDrawing}>ストップ</button>
+        </div>
         <div className='parent-block'>
-          <div className='childB-block'>
-            <img
-                src={this.state.imgPath}
-                className='imgData'
-                alt="cute dog"
-              />
-          </div>
           <div className='childA-block'>
             {this.state.posts.map((post,idx) => {
-              return <div key={idx}><div className='kanpaiName'>{post.title}さん</div><img className='kanpaiImg' src='https://1.bp.blogspot.com/-6Z3mOdbZ1kU/UZYlsWNbPDI/AAAAAAAATTk/nWPoBeyEazA/s800/beer_kanpai.png' alt="kanpai-img"/></div>})
+              return <div key={idx}><div className='questionTitle'>{post.title}</div></div>})
             }
           </div>
         </div>
